2e0a3db5d8309ae2f8df9a7099dce4dc93b25e68
wilkin <wilkin4@gmail.com>
2019-07-20 10:03:51 -0700

    hack to use simpler package.json

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -10,14 +10,14 @@
  * All rights reserved.
  */
 
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    cache = require('gulp-cached');
-
-gulp.task('jshint', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(cache('jshint', { optimizeMemory: true }))
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-summary'))
-        .pipe(jshint.reporter('fail'));
-});
+// var gulp = require('gulp'),
+//     jshint = require('gulp-jshint'),
+//     cache = require('gulp-cached');
+//
+// gulp.task('jshint', function () {
+//     return gulp.src('src/**/*.js')
+//         .pipe(cache('jshint', { optimizeMemory: true }))
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('jshint-summary'))
+//         .pipe(jshint.reporter('fail'));
+// });
