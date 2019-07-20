57915345ab38ef3a837c1635d63bb465f2a124f9
wilkin <wilkin4@gmail.com>
2019-07-20 11:03:00 -0700

    test of es6 feature

--- hack/Circles.js
+++ hack/Circles.js
@@ -1,14 +1,33 @@
+let path
 function onMouseDrag(event) {
     // The radius is the distance between the position
     // where the user clicked and the current position
     // of the mouse.
-    var path = new Path.Circle({
+    path = new Path.Circle({
         center: event.downPoint,
         radius: (event.downPoint - event.point).length,
         fillColor: 'white',
         strokeColor: 'black'
     });
 
+    path.fullySelected=true
+
     // Remove this path on the next drag event:
     path.removeOnDrag();
 };
+class MyClass { // test of using acorn
+    get message() {
+        return 'Haxxxllo Welt!'
+    }
+
+    saySomething() {
+        console.log(this.message)
+    }
+}
+
+function onMouseUp() {
+    if(path)
+        path.fullySelected=false
+    path=null
+    new MyClass().saySomething()
+}
