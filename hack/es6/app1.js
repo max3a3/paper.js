082dc5fe65d570eff6020e9364a8f244a5a900d9
wilkin <wilkin4@gmail.com>
2019-07-20 20:10:47 -0700

    can use babel to write test file with root at hack/app1.html

---
+++ hack/es6/app1.js
@@ -0,0 +1,44 @@
+function btn_exec_handler() {
+    console.log("btn_1")
+    // console.log($("#textinput").val())
+    let paper = window.paper
+
+    let ret = paper.execute($("#textinput").val())
+    console.log("result",ret)
+
+}
+async function btn_load_handler() {
+    const script_text = "a\nb\nc\n\nd"
+    $("#textinput").val(script_text)
+
+    let response = await axios.get('es6/app1_embed.js')
+    $("#textinput").val(response.data)
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+
+    var canvas = document.getElementById('canvas');
+    // Create an empty project and a view for the canvas:
+    paper.setup(canvas);
+
+    let style = {
+        fillColor: new paper.Color(1, 1, 0),
+        strokeColor: 'black',
+        strokeWidth: 2
+    };
+
+    let start_pt = [70, 5]
+    let start = new paper.Point(start_pt);
+    // Move to start and draw a line from there
+    let path = new paper.Path();
+    path.moveTo(start);
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([200, 250]));
+
+    path.style = style  // necessary?
+
+    $("#btn_exec").click(()=>btn_exec_handler());
+    $("#btn_load").click(()=>btn_load_handler());
+})
+
---
+++ hack/run_es6_babel.bat
@@ -0,0 +1,1 @@
+babel --watch es6 --out-dir js --source-maps
